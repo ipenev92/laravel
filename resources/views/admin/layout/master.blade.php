bb18ef72aae3e75a1190547839e7f72b4855f31d
Ivan Penev <ivan.penev@kaicen.com>
2023-11-22 11:56:45 +0100

    small revision

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -2,15 +2,13 @@
 <html lang="en">
 
 <head>
-    <!-- Esta línea para la codificación de carácteres -->
     <meta charset="utf-8">
-    <!-- Esta línea es para garantizar un diseño responsive que se adapte a todas las pantallas -->
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="shortcut icon" href="https://www.youtube.com/s/desktop/12d6b690/img/favicon.ico">
 
-    <title>Título</title>
+    <title>@yield('title')</title>
     <meta name="description" content="descripción de la web, se recomienda 90 caracteres">
-    <meta name="keywords" 	 content="palabras clave, separadas, por comas">
+    <meta name="keywords" content="palabras clave, separadas, por comas">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -39,8 +37,8 @@
     <script>
         function autoResize() {
             const textarea = document.getElementById('textarea');
-            textarea.style.height = 'auto'; // Reset height to auto
-            textarea.style.height = textarea.scrollHeight + 'px'; // Set the new height based on content
+            textarea.style.height = 'auto';
+            textarea.style.height = textarea.scrollHeight + 'px';
         }
     </script>
 </body>
