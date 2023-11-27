6d1e42e82fba070dab1a75a3274e2197690c3c80
Ivan <ivan.penev92@gmail.com>
2023-11-27 12:10:10 +0100

    post-build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 
 function autoResize() {
-    console.log('test');
     const textarea = document.getElementById("textarea");
     textarea.style.height = "auto";
     textarea.style.height = textarea.scrollHeight + "px";
